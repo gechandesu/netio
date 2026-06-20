7e09e394bb06c0f72be13ff8eb58182d51985510
ge <gechandev@gmail.com>
2026-06-20 06:39:09 +0300

    tests: fix unix_socket_test.v re-run

--- tests/unix_socket_test.v
+++ tests/unix_socket_test.v
@@ -1,4 +1,5 @@
 import netio
+import os
 
 fn test_unix_socket() {
 	mut socket := netio.Socket.new(netio.af_unix, netio.sock_stream, 0)!
@@ -7,6 +8,7 @@ fn test_unix_socket() {
 	} $else {
 		'/tmp/netio-test-unix.sock'
 	}
+	os.rm(addr) or {}
 	socket_addr := netio.SocketAddr.new_unix(addr)!
 	socket.bind(socket_addr)!
 	socket.close()!
